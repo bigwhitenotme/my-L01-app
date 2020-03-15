8cd89bd2826b9754908e94a88b099cb560cec15d
haymax <1664667015@qq.com>
2020-03-15 04:38:09 +0000

    静态文件浏览器缓存问题

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2020-03-14 19:04:46
- * @LastEditTime: 2020-03-15 11:56:37
+ * @LastEditTime: 2020-03-15 12:25:44
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \Code\my-L01-app\README.md
@@ -12,3 +12,4 @@ rm -rf node_modules
 rm -rf node_modules
 npm install --no-bin-links
 npm run dev --no-bin-links
+npm run watch-poll
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-03-14 19:04:47
+ * @LastEditTime: 2020-03-15 12:35:38
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \Code\my-L01-app\webpack.mix.js
+ */
 const mix = require('laravel-mix');
 
 /*
@@ -12,4 +20,4 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+  .sass('resources/sass/app.scss', 'public/css').version();
