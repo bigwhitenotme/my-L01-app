3f57c9d6ac17e4866ee759396525ac32623e7c83
haymax <1664667015@qq.com>
2020-03-15 04:47:18 +0000

    切割头部和底部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+  <a href="" target=_blank>
+    你连鸡蛋都画不好还想成为画家啊
+  </a>
+
+  <div class="float-right">
+    <a href="/about" >关于</a>
+  </div>
+</footer>
