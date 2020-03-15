70b375295688027ce8e8674b453ed6b04e3105f6
haymax <1664667015@qq.com>
2020-03-15 04:55:28 +0000

    命名路由

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,14 @@
 /*
  * @Author: your name
  * @Date: 2020-03-14 19:04:47
- * @LastEditTime: 2020-03-15 09:07:33
+ * @LastEditTime: 2020-03-15 12:53:37
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \Code\my-L01-app\routes\web.php
  */
-Route::get('/',    'StaticPagesController@home');
-Route::get('help', 'StaticPagesController@help');
-Route::get('about','StaticPagesController@about');
+Route::get('/',    'StaticPagesController@home')->name('home');
+Route::get('help', 'StaticPagesController@help')->name('help');
+Route::get('about','StaticPagesController@about')->name('about');
 //添加测试
 Route::get('/test','StaticPagesController@test');
 //
