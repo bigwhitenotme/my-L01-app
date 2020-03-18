242c6913e6ffc887f236a032a3147096b5382ee3
haymax <1664667015@qq.com>
2020-03-18 13:41:15 +0800

    社交统计信息

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call(UsersTableSeeder::class);
         $this->call(StatusesTableSeeder::class);
+        $this->call(FollowersTableSeeder::class);
+
         Model::reguard();
     }
 }
