f7911887908fd668febd9a17f41ecde460dc6f3b
haymax <1664667015@qq.com>
2020-03-18 13:32:16 +0800

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,7 +41,7 @@ class User extends Authenticatable
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
-
+    //每个用户可以发多条动态
     public function statuses()
     {
         return $this->hasMany(Status::class);
@@ -51,5 +51,37 @@ class User extends Authenticatable
     {
         return $this->statuses()
             ->orderBy('created_at', 'desc');
+    }
+    //多个粉丝
+    public function followers()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+    }
+    //多个关注
+    public function followings()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+    }
+
+    // 关注
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+    // 取消关注
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
     }
 }
